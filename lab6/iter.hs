9af184799300a73e081c330212cd632717aebb32
Kyril Khaletsky <zarrexx@gmail.com>
2017-11-04 14:12:02 +0000

    Update iter.hs

--- lab6/iter.hs
+++ lab6/iter.hs
@@ -1,6 +1,10 @@
+-- iter n f x applies function f n times with a starting value given by x
+
 iter :: Int -> (a -> a) -> a -> a
 iter 0 f x = x
 iter n f x = f (iter (n-1) f x)
+
+-- pow x n raises x to the power of n
 
 pow :: Int -> Int -> Int
 pow x n = iter n (*x) 1
